333695f1c8987e127cdb3d1a51cce7382192ed38
Jannik <mkawaganga@gmail.com>
2020-07-10 01:13:08 +0200

    undo change. this should be in teensy4 not teensy3

--- teensy3/core_pins.h
+++ teensy3/core_pins.h
@@ -33,7 +33,6 @@
 
 #include "kinetis.h"
 #include "pins_arduino.h"
-#include <stdbool.h>
 
 #define HIGH		1
 #define LOW		0
@@ -2254,7 +2253,6 @@ static inline uint8_t digitalReadFast(uint8_t pin)
 void pinMode(uint8_t pin, uint8_t mode);
 void init_pins(void);
 void analogWrite(uint8_t pin, int val);
-void flexPwmInvertPolarity(uint8_t pin, bool inversePolarity);
 uint32_t analogWriteRes(uint32_t bits);
 static inline uint32_t analogWriteResolution(uint32_t bits) { return analogWriteRes(bits); }
 void analogWriteFrequency(uint8_t pin, float frequency);
